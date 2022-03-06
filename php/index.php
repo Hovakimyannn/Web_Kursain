97cff7a47003e50b74d247af2756bdde50528c87
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2022-03-06 17:24:14 +0400

    a

--- php/index.php
+++ php/index.php
@@ -1,13 +1,11 @@
 <?php
 
-
-die('aaaaa');
 require_once 'DataBase.php';
 
 $dataBase = new DataBase();
 $dataBase->connect();
 $search = explode(' ', $_POST['search']);
-$models = $dataBase->queryLike("SELECT model FROM auto_shop.cars where name LIKE ?","%", $search);
+$models = $dataBase->queryLike("SELECT model FROM heroku_d1e343f1c933536.cars where name LIKE ?","%", $search);
 $arr = [];
 foreach ($models as $model) {
     $arr[] = $model['model'];
