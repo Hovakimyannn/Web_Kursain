2402af442e2476d5f23c294f015df40e4b5af3c7
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2022-03-06 14:03:14 +0400

    add project

---
+++ php/DataBase.php
@@ -0,0 +1,34 @@
+<?php
+
+class DataBase
+{
+    protected mysqli_stmt $stmt;
+    protected $db;
+
+    public function connect()
+    {
+        $mysqli = new mysqli('localhost', 'root', 'password', 'auto_shop');
+        if ($mysqli->connect_error) {
+            die('Connect Error (' . mysqli_connect_errno() . ') '
+                . mysqli_connect_error());
+        }
+        $this->db = $mysqli;
+    }
+
+    public function queryLike(string $query, string $reg, array $search)
+    {
+        $this->stmt = $this->db->prepare($query);
+        foreach ($search as $se) {
+            $reg .= $se . '%';
+        }
+        $this->stmt->bind_param('s', $reg);
+        $this->stmt->execute();
+        $result = $this->stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function close()
+    {
+        $this->stmt->close();
+    }
+}
--- php/index.php
+++ php/index.php
@@ -1,31 +1,16 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', 'password', 'auto_shop');
-if ($db->connect_error) {
-    exit('Cloud not connect');
-}
+require_once 'DataBase.php';
 
-$sql = "SELECT model FROM cars where name LIKE ?";
+$dataBase = new DataBase();
+$dataBase->connect();
 $search = explode(' ', $_POST['search']);
-$text = '%';
-foreach ($search as $se) {
-    $text .= $se . '%';
-}
-$stmt = $db->prepare($sql);
-$stmt->bind_param('s', $text);
-$stmt->execute();
-$result = $stmt->get_result();
-$models = $result->fetch_all(MYSQLI_ASSOC);
+$models = $dataBase->queryLike("SELECT model FROM auto_shop.cars where name LIKE ?","%", $search);
 $arr = [];
 foreach ($models as $model) {
     $arr[] = $model['model'];
 }
 $models = array_unique($arr);
-if (count($models) > 1) {
-    $model = '';
-} else {
-    $model = $models[0];
-}
-$stmt->close();
+$dataBase->close();
 
-header('location: http://your_domain/kursain/view/search_page.html' . '#' . $model);
+header('location: http://your_domain/kursain/view/search_page.html' . '#' . $models[0]);
