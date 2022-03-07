3c005ba1cda4ee4f1c28ea34f1c73dc654441a59
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2022-03-07 22:59:32 +0400

    a

--- php/search_page_back.php
+++ php/search_page_back.php
@@ -1,11 +1,11 @@
 <?php
 
 function queryEq() {
-    $mysqli = new mysqli('localhost', 'root', 'password', 'auto_shop');
+    $mysqli = new mysqli('us-cdbr-east-05.cleardb.net', 'b67fe0a625343b', 'f158ca13', 'heroku_d1e343f1c933536');
     if ($mysqli->connect_error) {
         die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
     }
-    $stmt = $mysqli->prepare("SELECT name, model, fuel, image FROM auto_shop.cars");
+    $stmt = $mysqli->prepare("SELECT name, model, fuel, image FROM heroku_d1e343f1c933536.cars");
     $stmt->execute();
     $result = $stmt->get_result();
     return $result->fetch_all(MYSQLI_ASSOC);
