ddf61eaf06153d28bbf1793c579b87ba5a2b2634
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2022-03-07 22:44:08 +0400

    a

--- php/index.php
+++ php/index.php
@@ -13,4 +13,4 @@ foreach ($models as $model) {
 $models = array_unique($arr);
 $dataBase->close();
 
-header('location: https://limitless-earth-58194.herokuapp.com/view/search_page.html' . '#' . $models[0]);
+header('location: ../view/search_page.php' . '#' . $models[0]);
