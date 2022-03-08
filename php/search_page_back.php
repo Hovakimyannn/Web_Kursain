b35990179578ff9b875f3b7dc2965352268425fa
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2022-03-08 12:01:15 +0400

    a

--- php/search_page_back.php
+++ php/search_page_back.php
@@ -1,11 +1,17 @@
 <?php
 
-function queryEq() {
+function queryEq($model = '')
+{
     $mysqli = new mysqli('us-cdbr-east-05.cleardb.net', 'b67fe0a625343b', 'f158ca13', 'heroku_d1e343f1c933536');
     if ($mysqli->connect_error) {
         die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
     }
-    $stmt = $mysqli->prepare("SELECT name, model, fuel, image FROM heroku_d1e343f1c933536.cars");
+    if ($model) {
+        $stmt = $mysqli->prepare("SELECT name, model, fuel, image FROM heroku_d1e343f1c933536.cars where model = ?");
+        $stmt->bind_param('s', $model);
+    } else {
+        $stmt = $mysqli->prepare("SELECT name, model, fuel, image FROM heroku_d1e343f1c933536.cars");
+    }
     $stmt->execute();
     $result = $stmt->get_result();
     return $result->fetch_all(MYSQLI_ASSOC);
