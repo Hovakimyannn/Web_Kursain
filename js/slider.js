6c216fae74d497c28433a8582c9c11240c91b670
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2022-03-11 21:59:37 +0400

    a

---
+++ js/slider.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let models = JSON.parse(this.responseText);
+        }
+    };
+    xhr.open("POST", "/php/get_all_name.php", true);
+    xhr.send();
+}
+
+let myIndex = 0;
+carousel();
+
+function carousel() {
+    let i;
+    let x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 9000);
+}
