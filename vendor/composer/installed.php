17c55451e0b3a2b1bd4d739411fc4410a4b694e2
Hovakimyannn <harutyunhovakimyan7@gmail.com>
2023-01-24 23:54:12 +0400

    fix

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
+        'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
+        'reference' => '9bc7c31655a648ddbdd6a3cb4843ad79f9be495f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '77e3edf7ff7f13427a7288e7063c1d31d6c8d1aa',
-        'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
+            'reference' => '9bc7c31655a648ddbdd6a3cb4843ad79f9be495f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '77e3edf7ff7f13427a7288e7063c1d31d6c8d1aa',
             'dev_requirement' => false,
         ),
     ),
